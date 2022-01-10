9c676a779a4dfc73619f3800fabbefa6dda530eb
RAMESH KUMAR THAKUR <89151608+RTSIR@users.noreply.github.com>
2022-01-10 21:15:15 +0530

    Add files via upload

---
+++ myConfig.py
@@ -0,0 +1,26 @@
+#import packages
+import os
+
+#default number of epochs
+epochs=30
+
+#default batch size for training
+batch_size=32
+
+#noise-level for training
+sigma=25.0  #change it according to noise level in your dataset
+
+#path to generate the data
+genDataPath='./data/CBSD432/'
+
+#path to save the genPatches
+save_dir='./data/'
+
+#path to training data
+data='./data/clean_pats_rgb.npy'
+
+#variables required to generate patch
+pat_size=30
+stride=10
+step=0
+
